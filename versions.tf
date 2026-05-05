957fc9b686959fc7fb114b0e5d9a858070b67471
alberto.hernandez <alberto@44t.tech>
2026-05-05 08:12:16 +0100

    Remove Backend Stuff

--- versions.tf
+++ versions.tf
@@ -8,12 +8,3 @@ terraform {
 
   required_version = ">= 1.15.1"
 }
-
-provider "aws" {
-  region  = var.aws_region
-  profile = var.aws_profile
-
-  default_tags {
-    tags = var.default_tags
-  }
-}
