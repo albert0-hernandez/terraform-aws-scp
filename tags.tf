fffea44528e32c16234d33cfd6001cf45575e837
alberto.hernandez <alberto@44t.tech>
2026-05-05 09:28:20 +0100

    Add AWS Config Rule

--- tags.tf
+++ tags.tf
@@ -10,3 +10,25 @@ resource "aws_organizations_policy_attachment" "tags" {
   policy_id = aws_organizations_policy.tags.id
   target_id = var.organization_id
 }
+
+
+locals {
+  _required_tags_map = { for index, v in var.compliance.tags : "tag${index}Key" => v }
+}
+
+resource "aws_config_config_rule" "required_tags" {
+  name = "required-tags"
+
+  source {
+    owner             = "AWS"
+    source_identifier = "REQUIRED_TAGS"
+  }
+
+  input_parameters = jsonencode(local._required_tags_map)
+
+  scope {
+    compliance_resource_types = [
+      "AWS::ECR::*",
+    ]
+  }
+}
